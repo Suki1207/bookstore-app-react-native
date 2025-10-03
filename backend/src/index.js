86e8a3a0630e966e2e5013e45b6d62b8f7c22f36
Suada <selimovicsuki@gmail.com>
2025-10-03 20:05:36 +0200

    Import cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 //const express = require('express');
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 
 import authRoutes from "./routes/authRoutes.js";
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 //console.log("PORT:", PORT);
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
