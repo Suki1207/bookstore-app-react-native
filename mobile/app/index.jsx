cbf69b4a6a4c4e67121084f1de3a6f481bd72a3c
Suada <selimovicsuki@gmail.com>
2025-10-04 18:30:46 +0200

    Setup root layout

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -10,6 +11,9 @@ export default function Index() {
       }}
     >
       <Text>Hello</Text>
+
+      <Link href="/(auth)/signup">Signup</Link>
+      <Link href="/(auth)">Login</Link>
     </View>
   );
 }
