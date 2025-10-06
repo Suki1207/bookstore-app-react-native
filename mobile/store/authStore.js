56a85a13c3c11b0899cb2ab859a6a35a90cbcfc0
Suada <selimovicsuki@gmail.com>
2025-10-06 22:52:09 +0200

    Add web service host render.com for hosting backend & Run cron job for sending request every 14 minutes

--- mobile/store/authStore.js
+++ mobile/store/authStore.js
@@ -10,13 +10,16 @@ const useAuthStore = create((set) => ({
     set({ isLoading: true });
 
     try {
-      const response = await fetch("http://localhost:3000/api/auth/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, email, password }),
-      });
+      const response = await fetch(
+        "https://bookstore-app-react-native.onrender.com/api/auth/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, email, password }),
+        }
+      );
 
       const data = await response.json();
 
