751db3701404f6c348d8a04246702e809a30c2a8
Suada <selimovicsuki@gmail.com>
2025-10-07 18:35:58 +0200

    Setup tabs and auto navigation

---
+++ mobile/app/(tabs)/profile.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Profile() {
+  return (
+    <View>
+      <Text>Profile tab</Text>
+    </View>
+  );
+}
--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,14 +1,36 @@
-import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeScreen from "../components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
 
+import useAuthStore from "../store/authStore";
+
 export default function RootLayout() {
+  const router = useRouter();
+  const segments = useSegments();
+  //console.log("segments", segments);
+
+  const { checkAuth, user, token } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  // Handle navigation based on authentication state
+  useEffect(() => {
+    const inAuthScreen = segments[0] === "(auth)";
+    const isSignedIn = user && token;
+
+    if (!isSignedIn && !inAuthScreen) router.replace("/(auth)");
+    else if (isSignedIn && inAuthScreen) router.replace("/(tabs)");
+  }, [user, token, segments]);
+
   return (
     <SafeAreaProvider>
       <SafeScreen>
         <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" />
+          <Stack.Screen name="(tabs)" />
           <Stack.Screen name="(auth)" />
         </Stack>
       </SafeScreen>
